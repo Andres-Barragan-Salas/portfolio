41fb00d031c845ea144c7cbe76ef783d1c73aeda
Andres-Barragan-Salas <A01026567@itesm.mx>
2022-10-22 15:25:24 -0500

    Setup for ESLint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import logo from './logo.svg';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +20,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
